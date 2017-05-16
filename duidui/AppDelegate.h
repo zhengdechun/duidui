9caa9ea9b7e10d237be542f5b984f0dd7e9befc8
apple <apple@appledeMacBook-Pro.local>
2017-05-16 13:35:20 +0800

    Initial Commit

---
+++ duidui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  duidui
+//
+//  Created by apple on 2017/5/16.
+//  Copyright © 2017年 huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ duidui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  duidui
+//
+//  Created by apple on 2017/5/16.
+//  Copyright © 2017年 huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
